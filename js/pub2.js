477b317b44431af793f36d5df0221e531b354350
seungki <oha103@naver,com>
2019-12-20 02:25:00 +0900

    12.20

--- js/pub2.js
+++ js/pub2.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
     $("#keyword").keyup(function() {
-        var k = $(this).val();
+        var k = $(this).val().toLowerCase();
         $("#contents > div > span").hide();
-        var temp = $("#contents > div > span > p:contains('" + k + "')");
-
-        $(temp).parent().show();
+        var temp = $("#contents > div > span > p");
+        var temp2 = new Array();
+        for(var i=0; i < temp.length ; i++){
+          if(temp[i].innerHTML.toLowerCase().includes(k)){
+            temp2.push(temp[i]);
+          }
+        }
+        $(temp2).parent().show();
     })
 })
